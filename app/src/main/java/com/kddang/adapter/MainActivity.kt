862c11cbae1caae041c7cd764fc9c2fc2ecc07e0
果仁 <582591725@qq.com>
2022-09-21 15:33:35 +0800

    上次gradle文件夹

--- app/src/main/java/com/kddang/adapter/MainActivity.kt
+++ app/src/main/java/com/kddang/adapter/MainActivity.kt
@@ -2,10 +2,12 @@ package com.kddang.adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kddang.library.abslistview.EasyLVHolder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
